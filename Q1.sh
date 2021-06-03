47b9c68f706b0c68153b63f94fde8372e157663c
PegahAmiriSani <39569670+PegahAmiriSani@users.noreply.github.com>
2021-06-03 02:02:59 -0700

    Add files via upload

---
+++ Q1.sh
@@ -0,0 +1,15 @@
+chmod 397
+
+mkdir file1
+chmod 440 file1
+
+mkdir file2
+chmod a=rx file2
+
+mkdir sample
+chmod g=w sample
+
+chmod r+x sample
+
+mkdir test
+chmod u+g test
---
+++ Q2.sh
@@ -0,0 +1,11 @@
+chmod --recursive
+
+chmod -v file1
+
+chmod -r file2
+
+chmod --version
+
+chmod -f newFile
+
+
